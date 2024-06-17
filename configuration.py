39d12d095428133a4fdaef59bcc25591a3123ba1
calint <calin.tenitchi@gmail.com>
2024-06-17 15:17:55 +0200

    .

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 #
 # if file changed run `configuration-generate.py`
 #
-RAM_ADDRESS_WIDTH = 21      # 2^21 = 2 MB PSRAM RAM
+RAM_ADDRESS_WIDTH = 21      # 2^21 = 2 MB PSRAM
 UART_BAUD_RATE = 9600       # 9600 baud, 8 bits, 1 stop bit, no parity
 CACHE_LINE_IX_BITWIDTH = 5  # 2^5*32 = 1 KB instruction and data cache
