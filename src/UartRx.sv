3c8ba87e5129d2d1f28e50df332277ee3a4ce3c8
calint <calin.tenitchi@gmail.com>
2024-06-18 13:04:24 +0200

    .

--- src/UartRx.sv
+++ src/UartRx.sv
@@ -19,14 +19,14 @@ module UartRx #(
 
   localparam BIT_TIME = CLK_FREQ / BAUD_RATE;
 
-  localparam STATE_IDLE = 0;
-  localparam STATE_START_BIT = 1;
-  localparam STATE_DATA_BITS = 2;
-  localparam STATE_STOP_BIT = 3;
-  localparam STATE_WAIT_GO_LOW = 4;
+  localparam STATE_IDLE = 5'b00001;
+  localparam STATE_START_BIT = 5'b00010;
+  localparam STATE_DATA_BITS = 5'b00100;
+  localparam STATE_STOP_BIT = 5'b01000;
+  localparam STATE_WAIT_GO_LOW = 5'b10000;
 
-  reg [$clog2(5)-1:0] state;
-  reg [$clog2(9)-1:0] bit_count;
+  reg [4:0] state;
+  reg [3:0] bit_count;  // 3 to fit number 8
   reg [(BIT_TIME == 1 ? 1 : $clog2(BIT_TIME))-1:0] bit_counter;
 
   always_ff @(posedge clk, negedge rst_n) begin
@@ -38,34 +38,43 @@ module UartRx #(
       dr <= 0;
     end else begin
       case (state)
+
         STATE_IDLE: begin
           if (!rx && go) begin  // does the cpu wait for data and start bit has started?
             bit_count <= 0;
             if (BIT_TIME == 1) begin
               // the start bit has been read, jump to data
-              bit_counter <= BIT_TIME - 1; // -1 because one of the ticks has been read before switching state
+              bit_counter <= BIT_TIME - 1;
+              // note: -1 because one of the ticks has been read before switching state
               state <= STATE_DATA_BITS;
             end else begin
               // get sample from half of the cycle
-              bit_counter <= BIT_TIME / 2 - 1; // -1 because one of the ticks has been read before switching state
+              bit_counter <= BIT_TIME / 2 - 1;
+              // note: -1 because one of the ticks has been read before switching state
               state <= STATE_START_BIT;
             end
           end
         end
+
         STATE_START_BIT: begin
-          if (bit_counter == 0) begin  // no check if rx==0 because there is no error recovery
-            bit_counter <= BIT_TIME - 1; // -1 because one of the ticks has been read before switching state
+          if (bit_counter == 0) begin
+            // note: no check if rx==0 because there is no error recovery
+            bit_counter <= BIT_TIME - 1;
+            // note: -1 because one of the ticks has been read before switching state
             state <= STATE_DATA_BITS;
           end else begin
             bit_counter <= bit_counter - 1;
           end
         end
+
         STATE_DATA_BITS: begin
           if (bit_counter == 0) begin
             data[bit_count] <= rx;
-            bit_counter <= BIT_TIME - 1; // -1 because one of the ticks has been read before switching state
+            bit_counter <= BIT_TIME - 1;
+            // note: -1 because one of the ticks has been read before switching state
             bit_count <= bit_count + 1;
-            if (bit_count == 7) begin  // 7, not 8, because of NBA of bit_count 
+            if (bit_count == 7) begin
+              // note: 7, not 8, because of NBA of bit_count 
               bit_count <= 0;
               state <= STATE_STOP_BIT;
             end
@@ -73,14 +82,17 @@ module UartRx #(
             bit_counter <= bit_counter - 1;
           end
         end
+
         STATE_STOP_BIT: begin
-          if (bit_counter == 0) begin  // no check if rx==1 because there is no error recovery
+          if (bit_counter == 0) begin
+            // note: no check if rx==1 because there is no error recovery
             dr <= 1;
             state <= STATE_WAIT_GO_LOW;
           end else begin
             bit_counter <= bit_counter - 1;
           end
         end
+
         STATE_WAIT_GO_LOW: begin
           if (!go) begin
             data <= 0;
@@ -88,6 +100,7 @@ module UartRx #(
             state <= STATE_IDLE;
           end
         end
+
       endcase
     end
   end
--- src/UartTx.sv
+++ src/UartTx.sv
@@ -21,14 +21,14 @@ module UartTx #(
 
   localparam BIT_TIME = CLK_FREQ / BAUD_RATE;
 
-  localparam STATE_IDLE = 0;
-  localparam STATE_START_BIT = 1;
-  localparam STATE_DATA_BITS = 2;
-  localparam STATE_STOP_BIT = 3;
-  localparam STATE_WAIT_GO_LOW = 4;
+  localparam STATE_IDLE = 5'b00001;
+  localparam STATE_START_BIT = 5'b00010;
+  localparam STATE_DATA_BITS = 5'b00100;
+  localparam STATE_STOP_BIT = 5'b01000;
+  localparam STATE_WAIT_GO_LOW = 5'b10000;
 
-  reg [$clog2(5)-1:0] state;
-  reg [$clog2(9)-1:0] bit_count;
+  reg [4:0] state;
+  reg [3:0] bit_count;  // 3 to fit number 8
   reg [(BIT_TIME == 1 ? 1 : $clog2(BIT_TIME))-1:0] bit_time_counter;
 
   always_ff @(negedge clk, negedge rst_n) begin
@@ -40,6 +40,7 @@ module UartTx #(
       bsy <= 0;
     end else begin
       case (state)
+
         STATE_IDLE: begin
           if (go) begin
             bsy <= 1;
@@ -48,9 +49,11 @@ module UartTx #(
             state <= STATE_START_BIT;
           end
         end
+
         STATE_START_BIT: begin
           if (bit_time_counter == 0) begin
-            bit_time_counter <= BIT_TIME - 1; // -1 because first 'tick' of the first bit is being sent in this state
+            bit_time_counter <= BIT_TIME - 1;
+            // note: -1 because first 'tick' of the first bit is being sent in this state
             tx <= data[0];  // start sending first bit of data
             bit_count <= 1;  // first bit is being sent during this cycle
             state <= STATE_DATA_BITS;
@@ -58,20 +61,23 @@ module UartTx #(
             bit_time_counter <= bit_time_counter - 1;
           end
         end
+
         STATE_DATA_BITS: begin
           if (bit_time_counter == 0) begin
             tx <= data[bit_count];
-            bit_time_counter <= BIT_TIME - 1; // -1 because first 'tick' of next bit is sent in this state
+            bit_time_counter <= BIT_TIME - 1;
+            // note: -1 because first 'tick' of next bit is sent in this state
             bit_count <= bit_count + 1;
             if (bit_count == 8) begin
               bit_count <= 0;
-              tx <= 1;  // start sending stop bit
+              tx <= 1;  // overwrite tx, start sending stop bit
               state <= STATE_STOP_BIT;
             end
           end else begin
             bit_time_counter <= bit_time_counter - 1;
           end
         end
+
         STATE_STOP_BIT: begin
           if (bit_time_counter == 0) begin
             bsy   <= 0;
@@ -80,11 +86,13 @@ module UartTx #(
             bit_time_counter <= bit_time_counter - 1;
           end
         end
+
         STATE_WAIT_GO_LOW: begin
           if (!go) begin  // wait for acknowledge that 'data' has been sent
             state <= STATE_IDLE;
           end
         end
+
       endcase
     end
   end
