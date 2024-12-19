d3e044cf815271ce1776fa6d053c636c7477096f
calint <calin.tenitchi@gmail.com>
2024-12-19 15:36:27 +0700

    .

--- os/qa/test.sh
+++ os/qa/test.sh
@@ -2,7 +2,7 @@
 set -e
 cd $(dirname "$0")
 
-../console_application < test.in > test.out
+echo -e "$(cat test.in)" | ../console_application > test.out
 
 if cmp -s test.diff test.out; then
     echo "test: OK"
