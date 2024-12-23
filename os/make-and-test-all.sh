ea9d53ba0aefeefc15af3b4f1c144b39dbf9d359
calint <calin.tenitchi@gmail.com>
2024-12-23 16:17:49 +0700

    .

--- os/make-and-test-all.sh
+++ os/make-and-test-all.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
+echo " * build console application"
 ./make-console-application.sh
+echo " * build fpga flash binary"
 ./make-fpga-flash-binary.sh
+echo " * test console application"
 qa-console/test.sh
+echo " * test os.bin using emulator"
 qa-emulator/test.sh
