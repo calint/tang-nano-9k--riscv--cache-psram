576d07eb87b45c33ecdc93936723f1476ae77093
calint <calin.tenitchi@gmail.com>
2024-12-24 01:00:20 +0700

    .

--- emulator/src/rv32i.hpp
+++ emulator/src/rv32i.hpp
@@ -38,7 +38,7 @@ public:
     uint32_t const opcode = OPCODE_from(instruction);
     switch (opcode) {
     //-----------------------------------------------------------------------
-    case 0x37: //                                                         LUI
+    case OPCODE_LUI: //                                                   LUI
     {
       uint32_t const rd = RD_from(instruction);
       uint32_t const U_imm20 = U_imm20_from(instruction);
@@ -49,63 +49,56 @@ public:
       break;
     }
     //-----------------------------------------------------------------------
-    case 0x13: //                                       logical ops immediate
+    case OPCODE_LOGICAL_IMM: //                         logical ops immediate
     {
       uint32_t const rs1 = RS1_from(instruction);
       uint32_t const rd = RD_from(instruction);
       int32_t const I_imm12 = I_imm12_from(instruction);
       uint32_t const funct3 = FUNCT3_from(instruction);
       switch (funct3) {
-      case 0x0: // ADDI
-      {
+      case FUNCT3_ADDI: {
 #ifdef RV32I_DEBUG
         std::printf("addi x%d, x%d, %d\n", rd, rs1, I_imm12);
 #endif
         regs_[rd] = regs_[rs1] + I_imm12;
         break;
       }
-      case 0x2: // SLTI
-      {
+      case FUNCT3_SLTI: {
 #ifdef RV32I_DEBUG
         std::printf("slti x%d, x%d, %d\n", rd, rs1, I_imm12);
 #endif
         regs_[rd] = int32_t(regs_[rs1]) < I_imm12 ? 1 : 0;
         break;
       }
-      case 0x3: // SLTIU
-      {
+      case FUNCT3_SLTIU: {
 #ifdef RV32I_DEBUG
         std::printf("sltiu x%d, x%d, %d\n", rd, rs1, I_imm12);
 #endif
         regs_[rd] = regs_[rs1] < uint32_t(I_imm12) ? 1 : 0;
         break;
       }
-      case 0x4: // XORI
-      {
+      case FUNCT3_XORI: {
 #ifdef RV32I_DEBUG
         std::printf("xori x%d, x%d, %d\n", rd, rs1, I_imm12);
 #endif
         regs_[rd] = regs_[rs1] ^ I_imm12;
         break;
       }
-      case 0x6: // ORI
-      {
+      case FUNCT3_ORI: {
 #ifdef RV32I_DEBUG
         std::printf("ori x%d, x%d, %d\n", rd, rs1, I_imm12);
 #endif
         regs_[rd] = regs_[rs1] | I_imm12;
         break;
       }
-      case 0x7: // ANDI
-      {
+      case FUNCT3_ANDI: {
 #ifdef RV32I_DEBUG
         std::printf("andi x%d, x%d, %d\n", rd, rs1, I_imm12);
 #endif
         regs_[rd] = regs_[rs1] & I_imm12;
         break;
       }
-      case 0x1: // SLLI
-      {
+      case FUNCT3_SLLI: {
         uint32_t const rs2 = RS2_from(instruction);
 #ifdef RV32I_DEBUG
         std::printf("slli x%d, x%d, %d\n", rd, rs1, rs2);
@@ -113,21 +106,18 @@ public:
         regs_[rd] = regs_[rs1] << rs2;
         break;
       }
-      case 0x5: // SRLI / SRAI
-      {
+      case FUNCT3_SRLI_SRAI: {
         uint32_t const rs2 = RS2_from(instruction);
         uint32_t const funct7 = FUNCT7_from(instruction);
         switch (funct7) {
-        case 0x0: // SRLI
-        {
+        case FUNCT7_SRLI: {
 #ifdef RV32I_DEBUG
           std::printf("srli x%d, x%d, %d\n", rd, rs1, rs2);
 #endif
           regs_[rd] = regs_[rs1] >> rs2;
           break;
         }
-        case 0x20: // SRAI
-        {
+        case FUNCT7_SRAI: {
 #ifdef RV32I_DEBUG
           std::printf("srai x%d, x%d, %d\n", rd, rs1, rs2);
 #endif
@@ -145,27 +135,24 @@ public:
       break;
     }
     //-----------------------------------------------------------------------
-    case 0x33: //                                                 logical ops
+    case OPCODE_LOGICAL: //                                       logical ops
     {
       uint32_t const rs1 = RS1_from(instruction);
       uint32_t const rs2 = RS2_from(instruction);
       uint32_t const rd = RD_from(instruction);
       uint32_t const funct3 = FUNCT3_from(instruction);
       switch (funct3) {
-      case 0x0: // ADD / SUB
-      {
+      case FUNCT3_ADD_SUB: {
         uint32_t const FUNCT7 = FUNCT7_from(instruction);
         switch (FUNCT7) {
-        case 0x0: // ADD
-        {
+        case FUNCT7_ADD: {
 #ifdef RV32I_DEBUG
           std::printf("add x%d, x%d, x%d\n", rd, rs1, rs2);
 #endif
           regs_[rd] = regs_[rs1] + regs_[rs2];
           break;
         }
-        case 0x20: // SUB
-        {
+        case FUNCT7_SUB: {
 #ifdef RV32I_DEBUG
           std::printf("sub x%d, x%d, x%d\n", rd, rs1, rs2);
 #endif
@@ -177,52 +164,45 @@ public:
         }
         break;
       }
-      case 0x1: // SLL
-      {
+      case FUNCT3_SLL: {
 #ifdef RV32I_DEBUG
         std::printf("sll x%d, x%d, x%d\n", rd, rs1, rs2);
 #endif
         regs_[rd] = regs_[rs1] << (regs_[rs2] & 0x1f);
         break;
       }
-      case 0x2: // SLT
-      {
+      case FUNCT3_SLT: {
 #ifdef RV32I_DEBUG
         std::printf("slt x%d, x%d, x%d\n", rd, rs1, rs2);
 #endif
         regs_[rd] = int32_t(regs_[rs1]) < int32_t(regs_[rs2]) ? 1 : 0;
         break;
       }
-      case 0x3: // SLTU
-      {
+      case FUNCT3_SLTU: {
 #ifdef RV32I_DEBUG
         std::printf("sltu x%d, x%d, x%d\n", rd, rs1, rs2);
 #endif
         regs_[rd] = regs_[rs1] < regs_[rs2] ? 1 : 0;
         break;
       }
-      case 0x4: // XOR
-      {
+      case FUNCT3_XOR: {
 #ifdef RV32I_DEBUG
         std::printf("xor x%d, x%d, x%d\n", rd, rs1, rs2);
 #endif
         regs_[rd] = regs_[rs1] ^ regs_[rs2];
         break;
       }
-      case 0x5: // SRL / SRA
-      {
+      case FUNCT3_SRL_SRA: {
         uint32_t const funct7 = FUNCT7_from(instruction);
         switch (funct7) {
-        case 0x0: // SRL
-        {
+        case FUNCT7_SRL: {
 #ifdef RV32I_DEBUG
           std::printf("srl x%d, x%d, x%d\n", rd, rs1, rs2);
 #endif
           regs_[rd] = regs_[rs1] >> (regs_[rs2] & 0x1f);
           break;
         }
-        case 0x20: // SRA
-        {
+        case FUNCT7_SRA: {
 #ifdef RV32I_DEBUG
           std::printf("sra x%d, x%d, x%d\n", rd, rs1, rs2);
 #endif
@@ -234,16 +214,14 @@ public:
         }
         break;
       }
-      case 0x6: // OR
-      {
+      case FUNCT3_OR: {
 #ifdef RV32I_DEBUG
         std::printf("or x%d, x%d, x%d\n", rd, rs1, rs2);
 #endif
         regs_[rd] = regs_[rs1] | regs_[rs2];
         break;
       }
-      case 0x7: // AND
-      {
+      case FUNCT3_AND: {
 #ifdef RV32I_DEBUG
         std::printf("and x%d, x%d, x%d\n", rd, rs1, rs2);
 #endif
@@ -256,7 +234,7 @@ public:
       break;
     }
     //-----------------------------------------------------------------------
-    case 0x23: //                                                       store
+    case OPCODE_STORE: //                                               store
     {
       using enum bus_op_width;
       uint32_t const rs1 = RS1_from(instruction);
@@ -265,8 +243,7 @@ public:
       uint32_t const address = regs_[rs1] + S_imm12;
       uint32_t const funct3 = FUNCT3_from(instruction);
       switch (funct3) {
-      case 0x0: // SB
-      {
+      case FUNCT3_SB: {
 #ifdef RV32I_DEBUG
         std::printf("sb x%d, %d(x%d)\n", rs2, S_imm12, rs1);
 #endif
@@ -276,8 +253,7 @@ public:
         }
         break;
       }
-      case 0x1: // SH
-      {
+      case FUNCT3_SH: {
 #ifdef RV32I_DEBUG
         std::printf("sh x%d, %d(x%d)\n", rs2, S_imm12, rs1);
 #endif
@@ -287,8 +263,7 @@ public:
         }
         break;
       }
-      case 0x2: // SW
-      {
+      case FUNCT3_SW: {
 #ifdef RV32I_DEBUG
         std::printf("sw x%d, %d(x%d)\n", rs2, S_imm12, rs1);
 #endif
@@ -303,7 +278,7 @@ public:
       break;
     }
     //-----------------------------------------------------------------------
-    case 0x3: //                                                         load
+    case OPCODE_LOAD: //                                                 load
     {
       using enum bus_op_width;
       uint32_t const rs1 = RS1_from(instruction);
@@ -312,8 +287,7 @@ public:
       uint32_t const address = regs_[rs1] + I_imm12;
       uint32_t const funct3 = FUNCT3_from(instruction);
       switch (funct3) {
-      case 0x0: // LB
-      {
+      case FUNCT3_LB: {
 #ifdef RV32I_DEBUG
         std::printf("lb x%d, %d(x%d)\n", rd, I_imm12, rs1);
 #endif
@@ -324,8 +298,7 @@ public:
         regs_[rd] = loaded & 0x80 ? 0xFFFFFF00 | loaded : loaded;
         break;
       }
-      case 0x1: // LH
-      {
+      case FUNCT3_LH: {
 #ifdef RV32I_DEBUG
         std::printf("lh x%d, %d(x%d)\n", rd, I_imm12, rs1);
 #endif
@@ -336,8 +309,7 @@ public:
         regs_[rd] = loaded & 0x8000 ? 0xFFFF0000 | loaded : loaded;
         break;
       }
-      case 0x2: // LW
-      {
+      case FUNCT3_LW: {
 #ifdef RV32I_DEBUG
         std::printf("lw x%d, %d(x%d)\n", rd, I_imm12, rs1);
 #endif
@@ -348,8 +320,7 @@ public:
         regs_[rd] = loaded;
         break;
       }
-      case 0x4: // LBU
-      {
+      case FUNCT3_LBU: {
 #ifdef RV32I_DEBUG
         std::printf("lbu x%d, %d(x%d)\n", rd, I_imm12, rs1);
 #endif
@@ -360,8 +331,7 @@ public:
         regs_[rd] = loaded;
         break;
       }
-      case 0x5: // LHU
-      {
+      case FUNCT3_LHU: {
 #ifdef RV32I_DEBUG
         std::printf("lhu x%d, %d(x%d)\n", rd, I_imm12, rs1);
 #endif
@@ -378,7 +348,7 @@ public:
       break;
     }
     //-----------------------------------------------------------------------
-    case 0x17: //                                                       AUIPC
+    case OPCODE_AUIPC: //                                               AUIPC
     {
       uint32_t const rd = RD_from(instruction);
       uint32_t const U_imm20 = U_imm20_from(instruction);
@@ -389,7 +359,7 @@ public:
       break;
     }
     //-----------------------------------------------------------------------
-    case 0x6F: //                                                         JAL
+    case OPCODE_JAL: //                                                   JAL
     {
       uint32_t const rd = RD_from(instruction);
       int32_t const J_imm20 = J_imm20_from(instruction);
@@ -402,7 +372,7 @@ public:
       break;
     }
     //-----------------------------------------------------------------------
-    case 0x67: //                                                        JALR
+    case OPCODE_JALR: //                                                 JALR
     {
       uint32_t const rs1 = RS1_from(instruction);
       uint32_t const rd = RD_from(instruction);
@@ -416,15 +386,14 @@ public:
       break;
     }
     //-----------------------------------------------------------------------
-    case 0x63: //                                                    branches
+    case OPCODE_BRANCH: //                                           branches
     {
       uint32_t const rs1 = RS1_from(instruction);
       uint32_t const rs2 = RS2_from(instruction);
       int32_t const B_imm12 = B_imm12_from(instruction);
       uint32_t const funct3 = FUNCT3_from(instruction);
       switch (funct3) {
-      case 0x0: // BEQ
-      {
+      case FUNCT3_BEQ: {
 #ifdef RV32I_DEBUG
         std::printf("beq x%d, x%d, 0x%x\n", rs1, rs2, pc_ + B_imm12);
 #endif
@@ -434,8 +403,7 @@ public:
         }
         break;
       }
-      case 0x1: // BNE
-      {
+      case FUNCT3_BNE: {
 #ifdef RV32I_DEBUG
         std::printf("bne x%d, x%d, 0x%x\n", rs1, rs2, pc_ + B_imm12);
 #endif
@@ -445,8 +413,7 @@ public:
         }
         break;
       }
-      case 0x4: // BLT
-      {
+      case FUNCT3_BLT: {
 #ifdef RV32I_DEBUG
         std::printf("blt x%d, x%d, 0x%x\n", rs1, rs2, pc_ + B_imm12);
 #endif
@@ -456,8 +423,7 @@ public:
         }
         break;
       }
-      case 0x5: // BGE
-      {
+      case FUNCT3_BGE: {
 #ifdef RV32I_DEBUG
         std::printf("bge x%d, x%d, 0x%x\n", rs1, rs2, pc_ + B_imm12);
 #endif
@@ -467,8 +433,7 @@ public:
         }
         break;
       }
-      case 0x6: // BLTU
-      {
+      case FUNCT3_BLTU: {
 #ifdef RV32I_DEBUG
         std::printf("bltu x%d, x%d, 0x%x\n", rs1, rs2, pc_ + B_imm12);
 #endif
@@ -477,8 +442,7 @@ public:
         }
         break;
       }
-      case 0x7: // BGEU
-      {
+      case FUNCT3_BGEU: {
 #ifdef RV32I_DEBUG
         std::printf("bgeu x%d, x%d, 0x%x\n", rs1, rs2, pc_ + B_imm12);
 #endif
@@ -560,6 +524,52 @@ public:
                : ((instruction >> 20) & 0x7FE) | ((instruction >> 9) & 0x800) |
                      (instruction & 0xFF000) | ((instruction >> 11) & 0x100000);
   }
+
+  static uint32_t constexpr OPCODE_LUI = 0x37;
+  static uint32_t constexpr OPCODE_LOGICAL_IMM = 0x13;
+  static uint32_t constexpr FUNCT3_ADDI = 0;
+  static uint32_t constexpr FUNCT3_SLTI = 2;
+  static uint32_t constexpr FUNCT3_SLTIU = 3;
+  static uint32_t constexpr FUNCT3_XORI = 4;
+  static uint32_t constexpr FUNCT3_ORI = 6;
+  static uint32_t constexpr FUNCT3_ANDI = 7;
+  static uint32_t constexpr FUNCT3_SLLI = 1;
+  static uint32_t constexpr FUNCT3_SRLI_SRAI = 5;
+  static uint32_t constexpr FUNCT7_SRLI = 0;
+  static uint32_t constexpr FUNCT7_SRAI = 0x20;
+  static uint32_t constexpr OPCODE_LOGICAL = 0x33;
+  static uint32_t constexpr FUNCT3_ADD_SUB = 0;
+  static uint32_t constexpr FUNCT7_ADD = 0;
+  static uint32_t constexpr FUNCT7_SUB = 0x20;
+  static uint32_t constexpr FUNCT3_SLL = 1;
+  static uint32_t constexpr FUNCT3_SLT = 2;
+  static uint32_t constexpr FUNCT3_SLTU = 3;
+  static uint32_t constexpr FUNCT3_XOR = 4;
+  static uint32_t constexpr FUNCT3_SRL_SRA = 5;
+  static uint32_t constexpr FUNCT7_SRL = 0;
+  static uint32_t constexpr FUNCT7_SRA = 0x20;
+  static uint32_t constexpr FUNCT3_OR = 6;
+  static uint32_t constexpr FUNCT3_AND = 7;
+  static uint32_t constexpr OPCODE_STORE = 0x23;
+  static uint32_t constexpr FUNCT3_SB = 0;
+  static uint32_t constexpr FUNCT3_SH = 1;
+  static uint32_t constexpr FUNCT3_SW = 2;
+  static uint32_t constexpr OPCODE_LOAD = 3;
+  static uint32_t constexpr FUNCT3_LB = 0;
+  static uint32_t constexpr FUNCT3_LH = 1;
+  static uint32_t constexpr FUNCT3_LW = 2;
+  static uint32_t constexpr FUNCT3_LBU = 4;
+  static uint32_t constexpr FUNCT3_LHU = 5;
+  static uint32_t constexpr OPCODE_AUIPC = 0x17;
+  static uint32_t constexpr OPCODE_JAL = 0x6F;
+  static uint32_t constexpr OPCODE_JALR = 0x67;
+  static uint32_t constexpr OPCODE_BRANCH = 0x63;
+  static uint32_t constexpr FUNCT3_BEQ = 0;
+  static uint32_t constexpr FUNCT3_BNE = 1;
+  static uint32_t constexpr FUNCT3_BLT = 4;
+  static uint32_t constexpr FUNCT3_BGE = 5;
+  static uint32_t constexpr FUNCT3_BLTU = 6;
+  static uint32_t constexpr FUNCT3_BGEU = 7;
 };
 
 } // namespace rv32i
