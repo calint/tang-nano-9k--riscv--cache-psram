4da7bb926ea36e80db3d19f000e88828dd5a4ef4
calint <calin.tenitchi@gmail.com>
2024-12-30 03:02:03 +0700

    .

--- configuration.py
+++ configuration.py
@@ -3,13 +3,17 @@
 #
 
 RAM_ADDRESS_BITWIDTH = 21
-# 2^21 = 2 MB PSRAM (according to hardware)
+# 2 ^ 21 = 2 MB PSRAM (according to hardware)
 
 UART_BAUD_RATE = 115200
 # 115200 baud, 8 bits, 1 stop bit, no parity
 
 CACHE_LINE_INDEX_BITWIDTH = 5
-# 2^5*32 = 1 KB unified instruction and data cache
+# 2 ^ 5 * 32 = 1 KB unified instruction and data cache
+# from 1 to 5: cache implemented with SSRAM
+#           6: leads to excessive build time
+#           7: cache implemented with some BSRAM
+#           8: implemented with some BSRAM but fails to place
 
 FLASH_TRANSFER_BYTES = 0x0020_0000
 # number of bytes to transfer from flash at startup (2 MB)
