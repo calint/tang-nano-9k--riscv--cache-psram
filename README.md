7763fce1cc45519d2e01ff9ac91213ca92465a61
calint <calin.tenitchi@gmail.com>
2024-12-30 18:55:48 +0700

    .

--- README.md
+++ README.md
@@ -77,6 +77,7 @@ welcome to adventure #4
 ## Todo
 ```
 -------------------------------------------------------------------------------------------------------------
+[ ] samples/echo: study why lhu, slli then srai instead of lw
 [x] uarttx: run on posedge clk
 [ ] os: backspace to ctrl+h (0x08) and update putty terminal configuration screenshot
     => move the 0x08 definition to console_application.cpp and 0x7f to os.cpp
--- notes/samples/echo/echo.c
+++ notes/samples/echo/echo.c
@@ -1,15 +1,13 @@
-#include "os_config.h"
+#define LED ((short volatile *)0xfffffffe)
+#define UART_OUT ((short volatile *)0xfffffffc)
+#define UART_IN ((short volatile *)0xfffffffa)
+#define MEMORY_TOP 0x200000
 
-static const char *hello = "Hello World\r\n";
-
-void uart_send_str(const char *str);
 void uart_send_char(char ch);
 char uart_read_char();
 
 extern "C" void run() {
   *LED = 0; // all leds on
-
-  uart_send_str(hello);
 
   while (1) {
     const char ch = uart_read_char();
@@ -21,16 +19,8 @@ extern "C" void run() {
   }
 }
 
-void uart_send_str(const char *str) {
-  while (*str) {
-    while (*UART_OUT != -1)
-      ;
-    *UART_OUT = *str;
-    str++;
-  }
-}
-
 void uart_send_char(const char ch) {
+  // wait for UART to be idle
   while (*UART_OUT != -1)
     ;
   *UART_OUT = ch;
@@ -38,6 +28,7 @@ void uart_send_char(const char ch) {
 
 char uart_read_char() {
   short ch = 0;
+  // wait for UART to read receive data
   while ((ch = *UART_IN) == -1)
     ;
   return char(ch);
