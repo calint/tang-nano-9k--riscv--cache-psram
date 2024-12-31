6850f01775c60dfa914ba86f92e8b5fb88ab4013
calint <calin.tenitchi@gmail.com>
2024-12-31 22:56:23 +0700

    .

--- build-using-gowin.sh
+++ build-using-gowin.sh
@@ -4,7 +4,7 @@ cd $(dirname "$0")
 
 ./configuration-apply.py
 
-gw_sh << EOF | grep -E 'WARN|ERROR'
+gw_sh << EOF | grep -E 'WARN|ERROR|Bitstream generation completed'
 open_project riscv.gprj
 run all
 EOF
