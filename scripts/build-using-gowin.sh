cbd04222be922e76fbdff8ed57a62842e12e2951
calint <calin.tenitchi@gmail.com>
2025-01-02 00:22:04 +0700

    .

--- scripts/build-using-gowin.sh
+++ scripts/build-using-gowin.sh
@@ -4,10 +4,14 @@ cd $(dirname "$0")
 
 cd ..
 
+rm -rf impl/pnr/
+
 ./configuration-apply.py
 
 gw_sh << EOF | grep -E 'WARN|ERROR|Bitstream generation completed'
 open_project riscv.gprj
 run all
 EOF
+
+ls -l impl/pnr/riscv.fs
 
