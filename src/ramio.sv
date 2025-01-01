12adc0627ebad0db6b925c3ef8738ab12f65e6c2
calint <calin.tenitchi@gmail.com>
2025-01-01 17:34:25 +0700

    .

--- src/ramio.sv
+++ src/ramio.sv
@@ -295,12 +295,10 @@ module ramio #(
       // if read from UART then reset the read data to -1
       if (address == AddressUartIn && read_type != '0) begin
         uartrx_data_received <= -1;
-      end
-
-      if (uartrx_go && uartrx_data_ready) begin
+      end else if (uartrx_go && uartrx_data_ready) begin
         // !!! unclear why necessary for this to be in an 'else if' instead 
         // !!!  of stand-alone 'if' to avoid characters being dropped from 'uartrx'
-        // !!! note: issue does not happen in Gowin 19.10.03
+        // !!! note: issue does not happen in Gowin 19.10.03 as often
 
         // if UART has data ready then copy the data and acknowledge (uartrx_go = 0)
         //  note: read data can be overrun
