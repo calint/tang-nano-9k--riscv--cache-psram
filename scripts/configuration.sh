670bdd077b42bdbb7b4faf6b4565c6e52291bb06
calint <calin.tenitchi@gmail.com>
2025-01-02 13:32:11 +0700

    .

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -11,4 +11,4 @@ FIRMWARE_FILE_MAX_SIZE_BYTES=4194304
 # used to check if the bitstream size is within the limit of flash storage
 
 FIRMWARE_FLASH_OFFSET=0x00000000
-# used to specify the offset in the flash storage where the bitstream will be written
+# used to specify the offset in the flash storage where the firmware will be written
