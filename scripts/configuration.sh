0c5c30ea839b76ce14e95eeb664ee242a793866d
calint <calin.tenitchi@gmail.com>
2025-01-02 12:53:17 +0700

    .

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -1,6 +1,9 @@
 BOARD_NAME="tangnano9k"
 # used when flashing the bitstream to the FPGA
 
+BITSTREAM_FLASH_TO_EXTERNAL=0
+# 0 to flash the bitstream to the internal flash, 1 for the external flash
+
 BITSTREAM_FILE_MAX_SIZE_BYTES=4194304
 # used to check if the bitstream size is within the limit of flash storage
 
--- scripts/flash-fpga.sh
+++ scripts/flash-fpga.sh
@@ -5,14 +5,19 @@ cd $(dirname "$0")
 # default configuration
 BOARD_NAME="tangnano9k"
 BITSTREAM_FILE="impl/pnr/riscv.fs"
+BITSTREAM_FLASH_TO_EXTERNAL=0
 
 # override configuration
 . ./configuration.sh
 
 cd ..
 
+if [ "$BITSTREAM_FLASH_TO_EXTERNAL" -eq 1 ]; then
+    OPTS="--external-flash"
+fi
+
 echo
 echo "flashing '$BITSTREAM_FILE' to board '$BOARD_NAME'"
 echo
 
-openFPGALoader --board $BOARD_NAME --write-flash "$BITSTREAM_FILE"
+openFPGALoader --board $BOARD_NAME --write-flash $OPTS "$BITSTREAM_FILE"
