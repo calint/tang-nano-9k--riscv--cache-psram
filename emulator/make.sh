c0ef876bc677f57e18c73f903afea46775e774b3
calint <calin.tenitchi@gmail.com>
2025-03-10 17:37:08 +0100

    .

--- emulator/make.sh
+++ emulator/make.sh
@@ -6,10 +6,11 @@
 set -e
 cd $(dirname "$0")
 
-CMD="g++ -std=c++23 -O3 $@ -fno-rtti -fno-exceptions -Wfatal-errors -Werror -Wall -Wextra -Wpedantic \
+CMD="g++ -std=c++23 -O3 -fno-rtti -fno-exceptions -Wfatal-errors -Werror -Wall -Wextra -Wpedantic \
     -Wconversion -Wsign-conversion -Wswitch-default \
     -o osqa src/main.cpp"
 #echo
 #echo $CMD
+#echo
 $CMD
 ls -la --color osqa
