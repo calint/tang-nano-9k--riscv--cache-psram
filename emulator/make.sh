115a0049ad0218f6da07329b62a1025914506ffe
calint <calin.tenitchi@gmail.com>
2025-03-10 17:27:01 +0100

    .

--- emulator/make.sh
+++ emulator/make.sh
@@ -7,8 +7,7 @@ set -e
 cd $(dirname "$0")
 
 CMD="g++ -std=c++23 -O3 $@ -fno-rtti -fno-exceptions -Wfatal-errors -Werror -Wall -Wextra -Wpedantic \
-    -Wswitch-default -Wconversion -Wsign-conversion \
-    -Wno-unused-parameter \
+    -Wconversion -Wsign-conversion -Wswitch-default \
     -o osqa src/main.cpp"
 #echo
 #echo $CMD
