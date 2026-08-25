99dc1978e668587f96cba37ac6e44811f59b6f25
calint <calin.tenitchi@gmail.com>
2026-08-25 15:53:50 +0200

    .

--- os/qa-emulator/test.sh
+++ os/qa-emulator/test.sh
@@ -2,12 +2,14 @@
 set -e
 cd $(dirname "$0")
 
-echo "* building emulator"
-../../emulator/make.sh
-
 EMULATOR=../../emulator/osqa
 FIRMWARE=../os.bin
 SDCARD=../../notes/samples/sample.txt
+
+echo "* building emulator"
+../../emulator/make.sh
+echo "* building firmware image"
+../make-fpga-flash-binary.sh
 
 echo " * running test for 2 seconds"
 echo -e "$(cat test.in)" | timeout 2 $EMULATOR $FIRMWARE $SDCARD >test.out || true
@@ -16,5 +18,5 @@ if cmp --silent test.diff test.out; then
     echo "test: PASSED"
     rm test.out
 else
-    echo "test: FAILED, check 'diff test.diff test.out'"
+    echo "test: FAILED, check 'diff --text test.diff test.out'"
 fi
